1e82f9170eaca414c5e5e17c2dfdfe1994db3b25
Avi <github.com@firehose.us>
2016-04-05 11:05:12 -0500

    Version bump.

--- robin.user.js
+++ robin.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         parrot (color multichat for robin!)
 // @namespace    http://tampermonkey.net/
-// @version      2.32
+// @version      2.68
 // @description  Try to take over the world!
 // @author       /u/_vvvv_
 // @include      https://www.reddit.com/robin*
